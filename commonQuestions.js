ad231325b2fb953e9faee06bcd1c941aefa20df1
supragyashrestha <supragyashrestha02@gmail.com>
2024-05-07 23:42:43 +0530

    common practice questions added

---
+++ commonQuestions.js
@@ -0,0 +1,59 @@
+/* 
+Typical Time - 1-1.5hr
+
+A -> Comment Widget (level 1 -> React, level 2 -> Vanilla JS)
+
+1. Implement a comment section with the following.
+    a. Add a comment, b. Edit a comment, c. delete a comment, d. reply a comment
+2. Display the latest 4 comments and its replies and remaining should be hidden and viewed with previous comment link
+3. On page, refresh reload the comment section
+4. Good to have n level of replies
+- Prioritise Object oriented design
+- Do not use external libraries
+
+
+B -> TO Do Lists of tasks (Similar to Jira board) (Vanilla JS)
+
+1. Lists of Tasks
+2. Edit List Name
+3. Input to add Task
+4. Edit and Delete any task in a list
+5. Drag and Drop of tasks
+
+
+C -> Food Ordering App
+
+1. Api integrate to fetch data ([{photo, name, description, rating, eta, tags: [{cuisine}], menu: [{food}]}])
+2. Responsive
+3. Search (** debouncing)
+4. Sort (name, rating, eta)
+5. Filter (cuisine tags)
+
+
+D -> E-mail Cient like Gmail
+
+1. Create your own data
+2. Design Complete Ui - 3 columns -> (Inbox, Sent, Starred) - Selected Email Lists in category - Selected Email
+3. Email -> Subject, sender/reciever, read/unread
+4. Unread/Read Count on Category as highlight
+5. Persist Data
+6. Delete Email
+7. Star Email
+
+
+E -> Sudoku/Tic-Tac-Toe Game
+1. Creating Dynamic Board
+2. Add cell click logic (use event bubbling concept)
+3. Get Winner
+4. Persist Data
+5. Clear Board
+6. Undo Last Play
+
+
+F -> Comparion of Two Products apps
+1. Create Responsive UI
+2. Table Highlighting Differences of Various Types (bool, int, string)
+3. Highlight the better column in any row
+
+
+*/
